076a82a587178789534317d24fd87f9169c53fdf
JVjoao07 <102702359+JVjoao07@users.noreply.github.com>
2025-03-16 14:28:37 -0300

    Feature Guerreiro

--- src/main.java
+++ src/main.java
@@ -1,5 +1,20 @@
+// Classe principal para testar o sistema
 public class Main {
     public static void main(String[] args) {
-        
+        Guerreiro guerreiro = new Guerreiro("Aragorn", 100, 1, 20, 15, "Espada");
+        Mago mago = new Mago("Gandalf", 80, 1, 25, 100, "Bola de Fogo");
+
+        // Testando as funcionalidades
+        guerreiro.atacar();
+        guerreiro.defender();
+        guerreiro.receberDano(30);
+        guerreiro.subirNivel();
+
+        System.out.println("\n-------------------\n");
+
+        mago.atacar();
+        mago.recuperarMana();
+        mago.receberDano(20);
+        mago.subirNivel();
     }
 }
